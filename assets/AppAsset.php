79d79412c7783121ecad4229e13e1f6c020ce617
Omar Virili <omar.virili@gmail.com>
2019-01-18 08:07:56 -0300

    luego de modificar los archivos para bs4 ya funcionando

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -17,12 +17,26 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+   // public $basePath = '@webroot';
+    //public $baseUrl = '@web';
+    public $sourcePath = '@bower/freelancer/';
     public $css = [
-        'css/site.css',
+        'vendor/bootstrap/css/bootstrap.min.css',
+        'vvendor/fontawesome-free/css/all.min.css',
+        'https://fonts.googleapis.com/css?family=Montserrat:400,700',
+        'https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic',
+        'vendor/magnific-popup/magnific-popup.css',
+        'css/freelancer.min.css',
     ];
     public $js = [
+            'vendor/jquery/jquery.min.js',
+            'vendor/bootstrap/js/bootstrap.bundle.min.js',
+            'vendor/jquery-easing/jquery.easing.min.js',
+            'vendor/magnific-popup/jquery.magnific-popup.min.js',
+            'js/jqBootstrapValidation.js',
+            'js/contact_me.js',
+            'js/freelancer.min.js',
+
     ];
     public $depends = [
         'yii\web\YiiAsset',
